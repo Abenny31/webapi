668383cdb055f66d143cfac5fa5d9af68e6088d2
Abenny31 <ante_benic@hotmail.com>
2021-03-08 22:52:01 +0100

    finished

---
+++ webapi1/Data/BlogPostsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using webapi1.Models;
+
+namespace webapi1.Data
+{
+    public class BlogPostsContext : DbContext
+    {
+        public BlogPostsContext (DbContextOptions<BlogPostsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+    }
+}
---
+++ webapi1/Startup.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using webapi1.Data;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+
+namespace webapi1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+         
+            services.AddMvc();
+            services.AddDbContext<BlogPostsContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("BlogPostsContext")));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials()
+                );
+            });
+            services.AddScoped(typeof(IDataRepository<>), typeof(DataRepository<>));
+
+            services.AddRouting();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseCors("CorsPolicy");
+
+            app.UseHttpsRedirection();
+            
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints=>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            }
+            );
+
+
+            app.UseSpa(spa =>
+            {
+              
+
+                spa.Options.SourcePath = @"C:\Radna Mapa\VS\webapi1\ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+
+
+        }
+    }
+}
